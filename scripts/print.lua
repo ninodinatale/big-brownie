32285613a6664c194cec61e9112ca3122d667d24
Nino Di Natale <nino.dinatale@talent-factory.ch>
2021-08-02 23:01:25 +0200

    added print utility

--- scripts/print.lua
+++ scripts/print.lua
@@ -1,16 +1,5 @@
 local Tinkr = ...
 
 local utils = Tinkr:require("scripts.big-brownie.modules.utils")
-local positioning = Tinkr:require("scripts.big-brownie.modules.positioning")
 
-local exports = Tinkr:require('Routine.Modules.Exports')
-
---utils.printMemberOf(exports, 'routine.txt')
-local playerX, playerY, playerZ = ObjectPosition('player')
-local toX, toY = ObjectPosition('target')
-
-local targetRadian = positioning.getTargetRadians(playerX, playerY, toX, toY)
-local playerRadian = GetPlayerFacing()
-local directionRadian = positioning.absoluteRadian(targetRadian - playerRadian)
-
-print(directionRadian)
+utils.printMemberOf(Tinkr.Routine, 'printed.txt')
